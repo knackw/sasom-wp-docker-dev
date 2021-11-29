2875390e7a427d67a4c65b188786b177b1abf39f
knackw <harald.schwankl@gmx.de>
2021-11-29 17:41:53 +0100

    some changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -23,23 +23,21 @@ if [ ! -f "wp-config.php" ]; then
         --dbname=${WORDPRESS_DB_NAME} \
         --dbuser=${WORDPRESS_DB_USER} \
         --dbpass=${WORDPRESS_DB_PASSWORD} \
-        --extra-php="
-/** Local Development */
-define('WP_CACHE_KEY_SALT', '${WORDPRESS_DOMAIN}');
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
-define( 'WP_HOME', 'https://${WORDPRESS_DOMAIN}' );
-define( 'WP_SITEURL', 'https://${WORDPRESS_DOMAIN}' );
-define( 'WP_MEMORY_LIMIT', '1024M' );
-define( 'DISABLE_WP_CRON', true);
-define( 'WP_CACHE', true);
-define( 'RT_WP_NGINX_HELPER_CACHE_PATH', '/var/run/NGINX-cache' );
-define( 'WP_REDIS_HOST', '192.168.32.3' );
-define( 'WP_REDIS_PORT', 6379 );
-define( 'WP_REDIS_PASSWORD', 'redispass' );
-define( 'WP_REDIS_TIMEOUT', 1 );
-define( 'WP_REDIS_READ_TIMEOUT', 1 );" \
-        ${ARGS}
+        --extra-php="/** Local Development */
+                     define('WP_CACHE_KEY_SALT', '${WORDPRESS_DOMAIN}');
+                     define( 'WP_DEBUG', true );
+                     define( 'WP_DEBUG_LOG', true );
+                     define( 'WP_HOME', 'https://${WORDPRESS_DOMAIN}' );
+                     define( 'WP_SITEURL', 'https://${WORDPRESS_DOMAIN}' );
+                     define( 'WP_MEMORY_LIMIT', '1024M' );
+                     define( 'DISABLE_WP_CRON', true);
+                     define( 'WP_CACHE', true);
+                     define( 'RT_WP_NGINX_HELPER_CACHE_PATH', '/var/run/NGINX-cache' );
+                     define( 'WP_REDIS_HOST', '192.168.32.3' );
+                     define( 'WP_REDIS_PORT', 6379 );
+                     define( 'WP_REDIS_PASSWORD', 'redispass' );
+                     define( 'WP_REDIS_TIMEOUT', 1 );
+                     define( 'WP_REDIS_READ_TIMEOUT', 1 );" \ ${ARGS}
 
     echo "Core Install"
     wp-cli core install \
@@ -65,12 +63,6 @@ define( 'WP_REDIS_READ_TIMEOUT', 1 );" \
     wp-cli ${ARGS} plugin activate flush-opcache
     wp-cli ${ARGS} plugin install https://downloads.wordpress.org/plugin/updraftplus.1.16.65.zip
     wp-cli ${ARGS} plugin activate updraftplus
-    #wp-cli ${ARGS} plugin install https://github.com/elegantthemes/divi-extension-example/archive/refs/heads/master.zip
-    #wp-cli ${ARGS} plugin activate divi-extension-example
-
-    # Theme Installation local example
-    #mv /tmp/divi/ wp-content/themes/divi/
-    #wp-cli ${ARGS} theme activate divi
 
     # Remove Plugin's
     wp-cli ${ARGS} plugin uninstall akismet
@@ -80,9 +72,6 @@ define( 'WP_REDIS_READ_TIMEOUT', 1 );" \
     wp-cli ${ARGS} theme activate twentynineteen
     wp-cli ${ARGS} theme uninstall twentytwenty
     wp-cli ${ARGS} theme uninstall twentytwentyone
-
-    # Set Options Example
-    #p-cli ${ARGS} option update et_divi '{ "et_automatic_updates_options": { "username": "", "api_key": "" } }' --format=json
 
     #Set Rewrite url
     wp-cli ${ARGS} rewrite structure '/%postname%/'
